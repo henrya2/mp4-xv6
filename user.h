a9cbf15308d6644e2f234c023b575cd2f19ecf36
Henry Read <henrya2@126.com>
2021-04-17 23:09:40 +0800

    adds declarions

--- user.h
+++ user.h
@@ -47,3 +47,7 @@ int atoi(const char*);
 
 int thread_create(void (*start_routine)(void *, void *), void *arg1, void *arg2);
 int thread_join();
+
+void lock_init(lock_t *lock);
+void lock_acquire(lock_t *lock);
+void lock_release(lock_t *lock);
